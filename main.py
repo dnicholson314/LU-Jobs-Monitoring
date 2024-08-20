fb0b1b243b1cae58ef102e873580c27ea6251185
dnicholson314 <143367057+dnicholson314@users.noreply.github.com>
2024-08-20 11:21:43 -0400

    Refactor key for retrieving posting dates

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
 import requests
-from datetime import  datetime as dt
+from datetime import datetime as dt
 
 URL = "https://careers.liberty.edu/wp-json/custom/v1/job-endpoint"
 TITLE_KEY = "jobreqJobTitle"
@@ -9,7 +9,7 @@ NUM_MOST_RECENT = 20
 response = requests.get(URL)
 jobs = response.json()
 
-by_date = lambda job: int(job["jobreqPostDateText"])
+by_date = lambda job: int(job[DATE_POSTED_KEY])
 sorted_jobs = sorted(jobs, key=by_date, reverse=True)
 cleaned_jobs = sorted_jobs[:NUM_MOST_RECENT-1]
 
@@ -17,7 +17,7 @@ for job in cleaned_jobs:
     name = job[TITLE_KEY] if TITLE_KEY in job else "No name"
 
     date_posted = "No date"
-    if "jobreqPostDateText" in job and job[DATE_POSTED_KEY] > 0:
-        date_posted = dt.fromtimestamp(job["jobreqPostDateText"])
+    if DATE_POSTED_KEY in job and job[DATE_POSTED_KEY] > 0:
+        date_posted = dt.fromtimestamp(job[DATE_POSTED_KEY])
 
     print(f"{name} ({date_posted})")
